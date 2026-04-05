cdaeb964f8cd1a5d92170ab0b721038d8d56af79
Keshav Pawar <keshavpawar784@gmail.com>
2026-04-05 18:32:05 +0530

    frontend deploy ready

--- frontend/src/components/KanbanBoard.jsx
+++ frontend/src/components/KanbanBoard.jsx
@@ -1,7 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { io } from "socket.io-client";
-
-const socket = io("http://localhost:5000");
+import { socket } from "../services/socket";
 
 export default function KanbanBoard() {
     const [tasks, setTasks] = useState([]);
