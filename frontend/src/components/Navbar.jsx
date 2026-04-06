05b401701d3939182be1602d9f3fc99b6ffddf70
Keshav Pawar <keshavpawar784@gmail.com>
2026-04-06 15:44:42 +0530

    implemented nabvar

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+export default function Navbar() {
+    return (
+        <nav className="flex justify-between items-center px-8 py-4 bg-white-400 text-white">
+            <h2 className="text-2xl font-bold text-teal-700">TaskFlow</h2>
+            <div className="space-x-4">
+                <button className="px-4 py-2 text-black text-lg rounded-lg hover:text-teal-600 hover:font-bold transition cursor-pointer">
+                    Github
+                </button>
+                <button className="px-4 py-2 text-black text-lg rounded-lg hover:text-black-100 hover:bg-teal-600 transition">
+                    Try Now
+                </button>
+            </div>
+        </nav>
+    )
+}
