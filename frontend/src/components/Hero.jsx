92f8f89369144df35349f71ea7bee329c7e5b59c
Keshav Pawar <keshavpawar784@gmail.com>
2026-04-06 22:45:57 +0530

    update hero of landing page

---
+++ frontend/src/components/Hero.jsx
@@ -0,0 +1,25 @@
+import kanban_board from "../assets/kanban_board.jpeg"
+
+export default function Hero(){
+    return (
+        <section className="flex flex-col md:flex-row items-center justify-between px-8 md:px-20 py-16 gap-10">
+            <div className="flex flex-col gap-8">
+            <div className="max-w-xl">
+                <h1 className="text-4xl md:text-5xl font-bold text-teal-600 leading-tight">Manage your tasks. <br /> Collaborate in real time</h1>
+                {/* <p>A fast and collaborative Kanban board built with React and Socket.io</p> */}
+            </div>
+            <div className="mt-4 flex gap-8">
+                <button className="text-xl text-black bg-teal-100 px-4 py-2 font-bold hover:bg-teal-600 hover:text-white rounded-lg">
+                    Try Now
+                </button>
+                <button className="text-lg text-black font-medium hover:text-teal-600  hover:underline decoration-2 transition duration-200">
+                    Github
+                </button>
+            </div>
+            </div>
+            <div>
+                <img src={kanban_board} alt="Kanban Board" className="w-full max-w-3xl rounded-xl shadow-2xl"></img>
+            </div>
+        </section>
+    )
+}
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,12 +1,13 @@
+
 export default function Navbar() {
     return (
-        <nav className="flex justify-between items-center px-8 py-4 bg-teal-100 text-white">
+        <nav className="flex justify-between items-center px-8 py-4 bg-teal-100">
             <h2 className="text-2xl font-bold text-teal-700">TaskFlow</h2>
             <div className="space-x-4">
-                <button className="px-4 py-2 text-black text-lg rounded-lg hover:text-teal-600 hover:font-bold transition cursor-pointer">
+                <button className="px-4 py-2 text-black text-lg rounded-lg hover:text-teal-800 hover:underline decoration-2 transition cursor-pointer">
                     Github
                 </button>
-                <button className="px-4 py-2 text-black text-lg rounded-lg hover:text-black-100 hover:bg-teal-600 transition">
+                <button className="px-4 py-2 text-black text-lg rounded-lg hover:text-white hover:bg-teal-600 transition duration-200">
                     Try Now
                 </button>
             </div>
