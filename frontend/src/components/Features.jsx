7356ebdc2e10d3aa1feec932eee9da88ac5e5950
Keshav Pawar <keshavpawar784@gmail.com>
2026-04-07 14:06:24 +0530

    fix hero and features alignment issues

--- frontend/src/components/Features.jsx
+++ frontend/src/components/Features.jsx
@@ -1,10 +1,10 @@
 export default function Features() {
     return (
-        <section className="flex flex-row md:flex-col items-start justify-between px-8 md:px-20 py-16 gap-10 bg-gray-50">
+        <section className="flex flex-row md:flex-col items-start justify-between px-8 md:px-20 py-16 gap-6 md:gap-10 bg-gray-50 text-center">
             <h2 className="text-4xl font-bold text-teal-500">Features</h2>
 
-            <div className="grid md:grid-cols-3 gap-8"> {/*all cards */}
-                <div className="p-6 bg-white rounded-xl shadow-md hover:shadow-lg transition hover:shadow-teal-200"> {/*single card*/}
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-8"> {/*all cards */}
+                <div className="p-6 bg-white rounded-xl shadow-md hover:shadow-lg transition hover:shadow-teal-200 max-w-auto max-w-sm"> {/*single card*/}
                     <h3 className="text-xl font-semibold">Real-time Sync</h3>
                     <p className="text-medium text-gray-700 mt-3">Tasks update instantly across multiple tabs</p>
                 </div>
